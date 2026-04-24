ae93d4c559522a225cbd4c41bf5a876b57bb3882
HumanRight19 <Dutaandra19@gmail.com>
2026-04-25 06:51:56 +0700

    compress media

--- src/data/videoData.js
+++ src/data/videoData.js
@@ -3,17 +3,17 @@ const videoData = [
     title: "Cooking Class",
     videos: [
       {
-        src: "/video/cooking-class/CK-1.m4v",
+        src: "/video/cooking-class/CK-1.mp4",
         instansi: "KB Darussalam II Pengkok",
         paket: "Cooking Class",
       },
       {
-        src: "/video/cooking-class/CK-2.m4v",
+        src: "/video/cooking-class/CK-2.mp4",
         instansi: "Kuttab Al Fath Al Islami",
         paket: "Cooking Class",
       },
       {
-        src: "/video/cooking-class/CK-3.m4v",
+        src: "/video/cooking-class/CK-3.mp4",
         instansi: "KB Cendana Kasih",
         paket: "Cooking Class",
       },
@@ -29,37 +29,37 @@ const videoData = [
     title: "Outbond",
     videos: [
       {
-        src: "/video/outbond/OB-1.m4v",
+        src: "/video/outbond/OB-1.mp4",
         instansi: "SMA Budi Utomo Solo",
         paket: "Outbond",
       },
       {
-        src: "/video/outbond/OB-2.m4v",
+        src: "/video/outbond/OB-2.mp4",
         instansi: "Posyandu Lansia Dewi Arimbi 3 Prayuan",
         paket: "Outbond",
       },
       {
-        src: "/video/outbond/OB-3.m4v",
+        src: "/video/outbond/OB-3.mp4",
         instansi: "Rumah Qur'an Sepanjang",
         paket: "Outbond",
       },
       {
-        src: "/video/outbond/OB-4.m4v",
+        src: "/video/outbond/OB-4.mp4",
         instansi: "BA Aisyiyah Jatirejo",
         paket: "Outbond",
       },
       {
-        src: "/video/outbond/OB-5.m4v",
+        src: "/video/outbond/OB-5.mp4",
         instansi: "TPA Al-Amin",
         paket: "Outbond",
       },
       {
-        src: "/video/outbond/OB-6.m4v",
+        src: "/video/outbond/OB-6.mp4",
         instansi: "Outbond",
         paket: "Outbond",
       },
       {
-        src: "/video/outbond/OB-7.m4v",
+        src: "/video/outbond/OB-7.mp4",
         instansi: "SDIT Tahfidzul Quran Mutiara Insan Sukoharjo",
         paket: "Outbond",
       },
@@ -69,7 +69,7 @@ const videoData = [
     title: "Parenting",
     videos: [
       {
-        src: "/video/paket-parenting/PAR-1.m4v",
+        src: "/video/paket-parenting/PAR-1.mp4",
         instansi: "BA Aisyiyah Ngambil-Ambil",
         paket: "Parenting",
       },
@@ -80,7 +80,7 @@ const videoData = [
     title: "Terusan 20K",
     videos: [
       {
-        src: "/video/paket-terusan-20k/T20.m4v",
+        src: "/video/paket-terusan-20k/T20.mp4",
         instansi: "Paket Terusan 20K",
         paket: "Terusan 20K",
       },
@@ -90,7 +90,7 @@ const videoData = [
     title: "Terusan 30K",
     videos: [
       {
-        src: "/video/paket-terusan-30K/T30.m4v",
+        src: "/video/paket-terusan-30k/T30.mp4",
         instansi: "Paket Terusan 30K",
         paket: "Terusan 30K",
       },
@@ -110,12 +110,12 @@ const videoData = [
     title: "Wisata Edukasi",
     videos: [
       {
-        src: "/video/wisata-edukasi/EDU-1.m4v",
+        src: "/video/wisata-edukasi/EDU-1.mp4",
         instansi: "TK Pertiwi Kaling 3",
         paket: "Wisata Edukasi",
       },
       {
-        src: "/video/wisata-edukasi/EDU-2.m4v",
+        src: "/video/wisata-edukasi/EDU-2.mp4",
         instansi: "TK MTA 1 Surakarta",
         paket: "Wisata Edukasi",
       },
@@ -125,22 +125,22 @@ const videoData = [
         paket: "Wisata Edukasi",
       },
       {
-        src: "/video/wisata-edukasi/EDU-4.m4v",
+        src: "/video/wisata-edukasi/EDU-4.mp4",
         instansi: "PAUD Nuri Kids School Sroyo",
         paket: "Wisata Edukasi",
       },
       {
-        src: "/video/wisata-edukasi/EDU-5.m4v",
+        src: "/video/wisata-edukasi/EDU-5.mp4",
         instansi: "RA Angkasa Jaya",
         paket: "Wisata Edukasi",
       },
       {
-        src: "/video/wisata-edukasi/EDU-6.m4v",
+        src: "/video/wisata-edukasi/EDU-6.mp4",
         instansi: "PAUD Ibnu Abas",
         paket: "Wisata Edukasi",
       },
       {
-        src: "/video/wisata-edukasi/EDU-7.m4v",
+        src: "/video/wisata-edukasi/EDU-7.mp4",
         instansi: "SDN 01 Klodran",
         paket: "Wisata Edukasi",
       },
@@ -152,6 +152,11 @@ const videoData = [
       {
         src: "/video/wisata-edukasi/EDU-9.mp4",
         instansi: "TK Aisyiyah 03 Ngringo",
+        paket: "Wisata Edukasi",
+      },
+      {
+        src: "/video/wisata-edukasi/EDU-10.mp4",
+        instansi: "TK Pertiwi 01 Delingan",
         paket: "Wisata Edukasi",
       },
     ],
